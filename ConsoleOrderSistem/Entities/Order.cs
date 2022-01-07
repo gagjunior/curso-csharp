6129e441cda6956426462165587b5ca4214fa169
Gilberto Junior <gagjunior@hotmail.com>
2022-01-07 12:50:50 -0300

    Código fonte do ConsoleOrderSistem

---
+++ ConsoleOrderSistem/Entities/Order.cs
@@ -0,0 +1,70 @@
+﻿using ConsoleOrderSistem.Entities.Enums;
+using System.Text;
+
+namespace ConsoleOrderSistem.Entities
+{
+    class Order
+    {
+        public Order() 
+        {
+        }
+
+        public DateTime Moment { get; set; }
+
+        public OrderStatus Status { get; set; }
+
+        public Client Client { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item) 
+        { 
+            if (Items.Contains(item))
+            {
+                Items.Remove(item);
+            }
+        }
+
+        public double Total()
+        {
+            double total = 0;
+            foreach (OrderItem item in Items)
+            {
+                total += item.SubTotal();
+            }
+            return total;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("ORDER SUMMARY");
+            sb.AppendLine($"Order Moment: {Moment.ToString("dd/MM/yyyy HH:mm:ss")}");
+            sb.AppendLine($"Order Staus: {Status}");
+            sb.AppendLine($"Client: {Client.Name} ({Client.BirthDate.ToString("dd/MM/yyyy")}) - {Client.Email}");
+            sb.AppendLine("Order Items");
+
+            foreach(OrderItem item in Items) 
+            { 
+                sb.AppendLine(item.ToString()); 
+            }
+
+            sb.AppendLine($"Total price: {Total():F2}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ConsoleOrderSistem/Program.cs
@@ -0,0 +1,48 @@
+﻿using ConsoleOrderSistem.Entities;
+using ConsoleOrderSistem.Entities.Enums;
+namespace ConsoleOrderSistem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**Enter cliente data**");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("E-mail: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("**Enter order data**");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int qtditems = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= qtditems; i++)
+            {
+                Console.WriteLine($"Enter {i}º item data:");
+                Console.Write($"Product name: ");
+                string nameProduct = Console.ReadLine();
+                Console.Write($"Product price: ");
+                double priceProduct = double.Parse(Console.ReadLine());
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(nameProduct, priceProduct);
+                OrderItem item = new OrderItem(quantity, priceProduct, product);
+
+                order.AddItem(item);
+            }
+
+            Console.WriteLine(order.ToString());
+            
+        }
+    }
+}
