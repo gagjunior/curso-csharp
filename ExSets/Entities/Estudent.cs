9cbfef2f9d09309b5ce1f8b0de4202d80b078e3c
Gilberto Junior <gagjunior@hotmail.com>
2022-01-22 23:02:04 -0300

    Exercicio sobre conjuntos

---
+++ ExSets/Entities/Estudent.cs
@@ -0,0 +1,29 @@
+﻿namespace ExSets.Entities
+{
+    internal class Estudent
+    {
+        public int Id { get; set; }
+
+        public Estudent(int id)
+        {
+            Id = id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Estudent))
+            {
+                return false;
+            }
+
+            Estudent other = obj as Estudent;
+
+            return Id.Equals(other.Id);
+        }
+    }
+}
---
+++ ExSets/Program.cs
@@ -0,0 +1,32 @@
+﻿using ExSets.Entities;
+
+HashSet<Estudent> estudents = new HashSet<Estudent>();
+
+Console.Write("How many students for course A? ");
+int numberOfStudents = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numberOfStudents; i++)
+{
+    int id = int.Parse(Console.ReadLine());
+    estudents.Add(new Estudent(id));
+}
+
+Console.Write("How many students for course B? ");
+numberOfStudents = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numberOfStudents; i++)
+{
+    int id = int.Parse(Console.ReadLine());
+    estudents.Add(new Estudent(id));
+}
+
+Console.Write("How many students for course C? ");
+numberOfStudents = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numberOfStudents; i++)
+{
+    int id = int.Parse(Console.ReadLine());
+    estudents.Add(new Estudent(id));
+}
+
+Console.WriteLine("Total students: " + estudents.Count);
